9b5e02ad68bb0b0e4ea47d8453e4055224208574
Boris Lau <boris@mobify.com>
2017-01-18 15:17:42 -0800

    Added test for add to cart

--- test_prod.sh
+++ test_prod.sh
@@ -1,3 +1,7 @@
 # The post endpoint
 curl -H "Content-Type: application/json" -X POST \
     -d '{"message":"Find me some eye of newt please"}' https://mobify-merlin-clippy.herokuapp.com/talk
+echo
+curl -H "Content-Type: application/json" -X POST \
+    -d '{"message":"add to cart"}' http://localhost:3000/talk/
+echo
