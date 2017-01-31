dc93b8b550c57eb3ff422d58660972e6c10c0a10
Boris Lau <boris@mobify.com>
2017-01-30 16:14:41 -0800

    Fix bad prod test link

--- test_prod.sh
+++ test_prod.sh
@@ -3,5 +3,5 @@ curl -H "Content-Type: application/json" -X POST \
     -d '{"message":"Find me some eye of newt please"}' https://mobify-merlin-clippy.herokuapp.com/talk
 echo
 curl -H "Content-Type: application/json" -X POST \
-    -d '{"message":"add to cart"}' http://localhost:3000/talk/
+    -d '{"message":"add to cart"}' http://mobify-merlin-clippy.herokuapp.com/talk
 echo
